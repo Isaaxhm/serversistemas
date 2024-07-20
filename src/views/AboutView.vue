b97fa1ad354c5525ac93e8dfe9660dcd1b991c5a
unknown <casigamerisaac@gmail>
2024-07-20 12:45:26 -0600

    Mensaje de confirmación

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Este es un texto</p>
+    <input type="text">
+    <input type="password">
+    <button type="button"> Ingresar</button>
+  </div>
+</template>
